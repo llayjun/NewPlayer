108974876a589551e2bcb5de10417207eea4d8cd
llayjun <1218543596@qq.com>
2017-12-22 15:14:57 +0800

    exit

--- app/src/main/java/nj/com/myplayer/MainActivity.java
+++ app/src/main/java/nj/com/myplayer/MainActivity.java
@@ -119,6 +119,7 @@ public class MainActivity extends BaseActivity implements PlayerManager.PlayerSt
             FileAnalyzeUtil.savePlayInfo2Shared(mFile.getPath());
             SPRollHelper.getInstance().clear();
             FileAnalyzeUtil.saveRollTextInfo2Shared(mFile.getPath());
+            ToastUtils.showToast(this, "文件扫描成功", Toast.LENGTH_LONG);
         }
     }
 
@@ -683,7 +684,8 @@ public class MainActivity extends BaseActivity implements PlayerManager.PlayerSt
                     }
                 }, 2000); // 如果2秒钟内没有按下返回键，则启动定时器取消掉刚才执行的任务
             } else {
-                moveTaskToBack(false);
+                finish();
+                System.exit(0);
             }
         } catch (Exception _e) {
             LogUtils.catchInfo(_e.toString());
